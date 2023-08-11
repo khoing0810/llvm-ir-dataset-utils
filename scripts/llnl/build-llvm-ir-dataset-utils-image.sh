5fc1d8f4d79a28c7424c317aa9c06acbd3d6ba23
Aiden Grossman <39388941+boomanaiden154@users.noreply.github.com>
2023-08-11 14:40:46 -0700

    Add llvm-tokenizer to dockerfile (#92)
    Add build script that I've been using on the clusters.
    Update gitignore to better work with the build script being in tree.
    Add llvm-tokenizer to Dockerfile, using a tar archive externally to avoid having git permissions inside the container to clone the repository.
    Remove spack from Dockerfile as I'm installing it separately rather than as part of the container due to permissions issues (probably something I should look into fixing soon).

---
+++ scripts/llnl/build-llvm-ir-dataset-utils-image.sh
@@ -0,0 +1,10 @@
+set -e
+git clone https://github.com/llvm-ml/llvm-tokenizer
+rm -rf ./llvm-tokenizer/.git
+tar -czf llvm-tokenizer.tar.gz ./llvm-tokenizer
+rm -rf ./llvm-tokenizer
+cp /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem ./tls-ca-bundle.crt
+podman build -t llvm-ir-dataset-utils -f ./.packaging/Dockerfile --build-arg="ENABLE_LEGACY_RENEGOTIATION=ON" --build-arg="CUSTOM_CERT=./tls-ca-bundle.crt" .
+podman save llvm-ir-dataset-utils > llvm-ir-dataset-utils.tar
+export SINGULARITY_CACHEDIR=/tmp/singularity-cache
+singularity build llvm-ir-dataset-utils.sif docker-archive:llvm-ir-dataset-utils.tar
